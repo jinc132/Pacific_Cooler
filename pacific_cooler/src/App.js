48c86fe81b99ed4170433c5e218b0413592f2eb1
jinc132 <jinc132@u.washington.edu>
2019-04-13 14:45:33 -0700

    Add routing skeleton

--- pacific_cooler/src/App.js
+++ pacific_cooler/src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, Collapse} from 'reactstrap';
 import './App.css';
+import SignIn from './Signin.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+      isOpen: false,
+    }
+  }
+
+  toggle() {
+    this.setState({isOpen: !this.state.isOpen});
+  }
+
   render() {
+    let renderFrontPage = () => {
+      return (
+        <div>
+
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+          <Switch>
+            <Route exact path={"/"} render={renderFrontPage} />
+            <Route path={"/SignIn"} Component={SignIn} />
+            <Redirect to={"/"} />
+          </Switch>
+        </Navbar>
       </div>
     );
   }
